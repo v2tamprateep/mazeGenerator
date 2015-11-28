6c4a9c0181fd70b3d73a6eff7f6615fcd2b2b229
Virin Tamprateep <v2tamprateep@gmail.com>
2015-11-27 18:05:47 -0600

    fixed while logic

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -2,15 +2,12 @@
 import sys, getopt
 import random
 import math
+import collections
 
-reverseAct = {"N": "S", "S": "N", "W": "E", "E": "W"}
+# reverseAct = {"N": "S", "S": "N", "W": "E", "E": "W"}
 actions = ['N', 'S', 'E', 'W']
 
 maze = {}
-# fill maze with walls ('%')
-for i in range(0, x):
-	for j in range(0, y):
-		maze[(i, j)] = '%'
 
 def nextPosition(position, direction):
 	if (direction is 'N'): return (position[0], position[1] + 1)
@@ -18,14 +15,25 @@ def nextPosition(position, direction):
 	if (direction is 'W'): return (position[0] - 1, position[1])
 	if (direction is 'S'): return (position[0], position[1] - 1)
 
-def isLegal(position, maze):
+def isLegal(position, maze, action):
 	x = position[0]
 	y = position[1]
 
-	if  (maze[(x+1, y-1)] is ' '): return False
-	if  (maze[(x+1, y+1)] is ' '): return False
-	if  (maze[(x-1, y-1)] is ' '): return False
-	if  (maze[(x-1, y+1)] is ' '): return False
+	if (action is not 'W'):
+		# if East is path
+		if  (maze[(x+1, y)] is not '%'): return False
+
+	if (action is not 'E'):
+		# if West is path
+		if  (maze[(x-1, y)] is not '%'): return False
+
+	if (action is not 'N'):
+		# if South is path
+		if  (maze[(x, y-1)] is not '%'): return False
+
+	if (action is not 'S'):
+		# if North is path
+		if  (maze[(x, y+1)] is not '%'): return False
 
 	return True
 
@@ -33,7 +41,7 @@ def getLegalActions(position):
 	legalMoves = []
 	for act in actions:
 		nextPos = nextPosition(position, act)
-		if isLegal(nextPos): legalMoves.append(act)
+		if isLegal(nextPos, act): legalMoves.append(act)
 	return legalMoves
 
 def backtrack():
@@ -44,26 +52,33 @@ def backtrack():
 
 def main():
 	try:
-		opts, arg = getopt.getopt(sys.arg[1:], "hx:y:", ["help"])
+		opts, arg = getopt.getopt(sys.argv[1:], "hx:y:", ["help"])
 	except getopt.GetoptError as err:
 		sys.exit(2)
 
-	x = 0
-	y = 0
+	x = 10
+	y = 10
 	for opt, arg in opts:
 		if opt == "x":
 			x = arg
 		if opt == "y":
 			y = arg
+	if (x is 0 or y is 0): sys.exit()
 
-	start = (round(random.uniform(1, x)), round(random.uniform(1, y)))
+	for i in range(0, x):
+		for j in range(0, y):
+			maze[(i, j)] = '%'
+
+	# start = (round(random.uniform(1, x)), round(random.uniform(1, y)))
+	start = (1, 1)
 	maze[start] = ' '
 	stack = [start]
-
-	while (len(stack) is 0):
+	print(len(stack))
+	while (len(stack) is not 0):
 		position = stack[len(stack)-1]
 
 		legalActions = getLegalActions(position)
+		print(legalActions)
 		if (len(legalActions) is 0):
 			backtrack()
 			continue
@@ -72,6 +87,18 @@ def main():
 		position = nextPosition(position, action)
 		maze[position] = ' '
 		stack.append(position)
+	"""
+	mazeFile = open('./test.txt', 'w')
+	for j in range(y-1, -1, -1):
+		for i in range(0, x):
+			mazeFile.write(maze[(i, j)])
+		mazeFile.write("\n")
+	"""
+
+	for j in range(y-1, -1, -1):
+		for i in range(0, x):
+			print(maze[(i, j)]),
+		print("")
 
 	print("end")
 	sys.exit()
